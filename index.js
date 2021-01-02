fc1c63b09d06dd2eee76903f2747f0a2a7121b3c
ShikharSingh98 <shikharsingh98@gmail.com>
2021-01-02 23:08:42 +0530

    Added index.js for animations

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function () {
+  document.getElementsByClassName('navbar')[0].style.animation =
+    'fadein 1s linear';
+  document.getElementById('introduction').style.animation =
+    'fadein 1.5s linear';
+});
