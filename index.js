258ce03f3ce8a2d1267b134a0bc82b227f57142e
ShikharSingh98 <shikharsingh98@gmail.com>
2021-01-15 12:50:55 +0530

    Removed the animation

--- index.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementsByClassName('navbar')[0].style.animation =
-    'fadein 1s linear';
-  document.getElementById('introduction').style.animation =
-    'fadein 1.5s linear';
-});
